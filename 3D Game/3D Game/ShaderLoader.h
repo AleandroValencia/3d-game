214150a5c12f82c524deda92768df87fb0df6f1c
Aleandro Valencia <axv10@hotmail.com>
2018-09-15 13:01:11 +1200

    Shader loader code from previous projects.

--- 3D Game/3D Game/ShaderLoader.h
+++ 3D Game/3D Game/ShaderLoader.h
@@ -2,11 +2,26 @@
 #define _SHADERLOADER_H__
 #pragma once
 
+#include "glew.h"
+#include "ShaderLoader.h"
+#include <iostream>
+#include<fstream>
+#include<vector>
+
 class ShaderLoader
 {
 public:
-	ShaderLoader();
-	~ShaderLoader();
+
+	ShaderLoader(void);
+	~ShaderLoader(void);
+	GLuint CreateProgram(char* VertexShaderFilename, char* FragmentShaderFilename);
+	GLuint CreateProgram(char* VertexShaderFilename, char* FragmentShaderFilename, char* GeometryShaderFilename);
+	GLuint CreateProgram(char* VertexShaderFilename, char* FragmentShaderFilename, char* TessControlShaderFilename, char* TessEvalShaderFilename);
+	GLuint CreateCompProgram(char* ComputeShaderFilename);
+
+private:
+	std::string ReadShader(char *filename);
+	GLuint CreateShader(GLenum shaderType, std::string source, char* shaderName);
 };
 
 #endif // !_SHADERLOADER_H__
